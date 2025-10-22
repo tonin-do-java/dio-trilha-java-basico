f3d5061f23e673b19e027d3b5e6aec632e9002b4
tonin-do-java <ant.fds.com@gmail.com>
2025-10-22 11:02:02 -0300

    terminado o curso de tipos de variáves

--- tipos-variáveis/src/VariaveisTipos.java
+++ tipos-variáveis/src/VariaveisTipos.java
@@ -1,5 +1,20 @@
 public class VariaveisTipos {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Antônio";
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(meuNome);
+
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
