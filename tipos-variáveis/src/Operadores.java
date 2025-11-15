49247568618aecbb635cfa462bfab65b5b0318be
tonin-do-java <ant.fds.com@gmail.com>
2025-11-15 00:34:16 -0300

    aula de operadores terminada

---
+++ tipos-variáveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String args[]) {
+        int idade = 20;
+
+        String status = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+        
+        System.out.println(status);
+    }
+
+}
